59716ccec63153a966c28ca8412f49912d11c2c6
Scott Patten <scott@scottpatten.ca>
2011-11-12 22:41:24 -0800

    adding mustache rendering inside a layout

--- lib/site_generator.rb
+++ lib/site_generator.rb
@@ -3,9 +3,11 @@ class SiteGenerator
   HTML_FILES_DIR = "/var/lpub/book_generator_repos/thes3cookbook_markdown/html_files"
   BOOK_FILE = "/var/lpub/book_generator_repos/thes3cookbook_markdown/manuscript/Book.txt"
   PUBLIC_DIR = File.expand_path('../../public', __FILE__)
+  LAYOUTS_DIR = File.expand_path('../../layouts', __FILE__) 
   BOOK_OUTPUT_DIR = File.expand_path('../../public/book', __FILE__)
   
   def initialize
+    @layout = File.read(File.join(LAYOUTS_DIR, 'site.html'))
   end
 
   def clear
@@ -19,13 +21,15 @@ class SiteGenerator
     write_book_files
   end
 
+  private
+  
   def setup_directories
     FileUtils.mkdir_p(BOOK_OUTPUT_DIR)
   end
 
   def write_index
     File.open(File.join(PUBLIC_DIR, 'index.html'), 'w') do |file|
-      file << "Hello"
+      file << content_in_layout("hello")
     end
   end
   
@@ -38,8 +42,12 @@ class SiteGenerator
       html_file = File.basename(file, File.extname(file)) + ".html"
       content = File.read(File.join(HTML_FILES_DIR, html_file))
       File.open(File.join(BOOK_OUTPUT_DIR, html_file), 'w') do |outfile|
-        outfile << content
+        outfile << content_in_layout(content)
       end
     end
+  end
+
+  def content_in_layout(content)
+    Mustache.render(@layout, :content => content)
   end
 end
